943fb7b7ee333c19ffa70015cac56b588e338bde
Pulkit Gupta <pulgupta@gmail.com>
2019-03-12 00:33:40 +0530

    added more tree problem

--- src/main/java/org/ds/algos/practice/ds_algos/trees/CheckIfTreeIsBalanced.java
+++ src/main/java/org/ds/algos/practice/ds_algos/trees/CheckIfTreeIsBalanced.java
@@ -9,19 +9,26 @@ public class CheckIfTreeIsBalanced {
 	}
 
 	public static boolean isHeightBalanced(Node root, Height height) {
+		// If root is null then height is zero and such tree is always balanced
 		if (root == null) {
 			height.height = 0;
 			return true;
 		}
 		Height lHeight = new Height();
 		Height rHeight = new Height();
+		// Check if the left sub tree is balanced
 		boolean l = isHeightBalanced(root.left, lHeight);
+		// Check if the right sub tree is balanced
 		boolean r = isHeightBalanced(root.right, rHeight);
-		int lh = lHeight.height, rh = rHeight.height;
+		// Get the left and right sub tree height
+		int lh = lHeight.height;
+		int rh = rHeight.height;
 
+		// The height of the current node is one more than max of left and right height
 		height.height = (lh > rh ? lh : rh) + 1;
 
-		if ((lh - rh >= 2) || (rh - lh >= 2))
+		// Check if the distance is not more than 1
+		if ((lh - rh > 1) || (rh - lh > 1))
 			return false;
 		else
 			return l && r;
