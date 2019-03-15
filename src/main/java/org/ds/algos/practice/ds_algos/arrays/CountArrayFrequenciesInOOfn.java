1486dbaa22d30c389c0b52ab8dace6a951b45c76
Pulkit Gupta <pulgupta@gmail.com>
2019-03-15 11:30:08 +0530

    added more problems

--- src/main/java/org/ds/algos/practice/ds_algos/arrays/CountArrayFrequenciesInOOfn.java
+++ src/main/java/org/ds/algos/practice/ds_algos/arrays/CountArrayFrequenciesInOOfn.java
@@ -1,20 +1,30 @@
 package org.ds.algos.practice.ds_algos.arrays;
 
+/**
+ * In an array of size n and containing numbers from 1...n calculate
+ * the frequencies of all the numbers
+ * @author pulgupta
+ */
 public class CountArrayFrequenciesInOOfn {
 	
 	public static int[] calculateFrequencies(int[] arr) {
-		
+		/*
+		 * The idea here is to store all the counts as negatives
+		 */
 		int i=0;
 		while(i < arr.length) {
+			// If the index has negative value then we already have a count 
 			if(arr[i]<0) {
 				i++;
 			} else {
 				int index = arr[i]-1;
+				// We can increment the count 
 				if(arr[index] < 0) {
 					arr[index]--;
 					arr[i]=0;
 					i++;
 				} else {
+					// Swap the number to current, and set -1 as the count
 					arr[i] = arr[index];
 					arr[index] = -1;
 				}
