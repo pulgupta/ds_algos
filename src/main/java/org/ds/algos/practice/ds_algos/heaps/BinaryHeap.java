3253be2ec55eca5e8f8df3e2fdf7b397dccdb333
Pulkit Gupta <pulgupta@gmail.com>
2019-04-27 14:44:57 +0530

    added a heap craetion algo and also palindrome check for a number

--- src/main/java/org/ds/algos/practice/ds_algos/heaps/BinaryHeap.java
+++ src/main/java/org/ds/algos/practice/ds_algos/heaps/BinaryHeap.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 public class BinaryHeap {
 	//This array will store the heap elements
-	static List<Integer> heap;
+	List<Integer> heap;
 	
-	static{
-		heap = new ArrayList<Integer>();
+	public BinaryHeap() {
+		heap = new ArrayList<>();
 	}
-	
 	//Returns the minimum element of the heap
 	public int getMin() {
 		return heap.get(0);
@@ -23,8 +22,74 @@ public class BinaryHeap {
 		return 0;
 	}
 	
+	public void createHeap(List<Integer> data) {
+		heap = data;	
+		// Lets start our heapify
+		int size = heap.size();
+		// If we have a heap of size n then leaf node starts from
+		// n/2. Hence we are starting from the last non leaf node.
+		// Also we start in a bottom up fashion, so we first create a 
+		// min heap at lower level and then we move towards the top
+		for(int i=size/2 -1; i>=0; i--) {
+			heapify(i, size);
+		}
+	}
+	
+	public int poll() {
+		int result = heap.get(0);
+		// Swap the root with the last element
+		heap.set(0, heap.get(heap.size()-1));
+		heap.remove(heap.size()-1);
+		heapify(0, heap.size());
+		return result;
+	}
+	
+	private void heapify(int i, int size) {
+		int minimum = i;
+		int l = 2*i + 1;
+		int r = 2*i + 2;
+		// Find the minimum child
+		if(l < size && heap.get(i) > heap.get(l))
+			minimum = l;
+		
+		if(r < size && heap.get(minimum) > heap.get(r))
+			minimum = r;
+		
+		// swap the minimum child with the root
+		if(minimum != i) {
+			int temp = heap.get(i);
+			heap.set(i, heap.get(minimum));
+			heap.set(minimum, temp);
+			// Recursively check all the lower levels where we have 
+			// pushed down the root
+			heapify(minimum, size);
+		}
+	}
+	
+	public void printHeap() {
+		for(int i=0; i<heap.size(); i++) {
+			System.out.print(heap.get(i));
+			if(i<heap.size()-1)
+				System.out.print("->");
+		}
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		//Main method to call the heap operations
+		BinaryHeap heap = new BinaryHeap();
+		List<Integer> list = new ArrayList<>();
+		list.add(6);
+		list.add(5);
+		list.add(9);
+		list.add(3);
+		list.add(1);
+		list.add(2);
+		heap.createHeap(list);
+		System.out.println("print heap after creation");
+		heap.printHeap();
+		
+		System.out.println("Smallest element is " + heap.poll());
+		heap.printHeap();
 		
 	}
 }
