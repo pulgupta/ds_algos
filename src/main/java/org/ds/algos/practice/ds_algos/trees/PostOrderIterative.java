f50d36641ea2b1968b545fa1a9bbde7d1fc9c6a2
Pulkit Gupta <pulgupta@gmail.com>
2019-05-13 08:49:35 +0530

    started post order iterative

---
+++ src/main/java/org/ds/algos/practice/ds_algos/trees/PostOrderIterative.java
@@ -0,0 +1,35 @@
+package org.ds.algos.practice.ds_algos.trees;
+
+import java.util.Stack;
+
+import org.ds.algos.practice.ds_algos.trees.Tree.Node;
+
+public class PostOrderIterative {
+
+	/* 		1      
+	 *	   / \     
+	 *    2   5    
+	 *   / \       
+	 *  3   4      
+	 */
+	private static void postOrderIterative(Node node) {
+		if(node == null)
+			return;
+		Stack<Node> stack = new Stack<Node>();
+		Node current = node;
+		while (!stack.isEmpty() || current!=null) {
+			while (current.left!=null) {
+				stack.push(current);
+				current = current.left;
+			}
+			Node processed = stack.pop();
+				
+		}
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Tree().createTree();
+		postOrderIterative(root);
+	}
+	
+}
