8f12f46ff616d18d9ada56cf11925a9bf52f82c5
Pulkit Gupta <pulgupta@gmail.com>
2019-06-19 16:45:24 +0530

    added more tree problems

---
+++ src/main/java/org/ds/algos/practice/ds_algos/trees/BstInsertDelete.java
@@ -0,0 +1,29 @@
+package org.ds.algos.practice.ds_algos.trees;
+
+public class BstInsertDelete {
+
+    /**
+     * Insert the node at the correct place making sure it is still a BST
+     * We will not be balancing the tree once the node is inserted
+     * Time Complexity :
+     * Space Complexity :
+     * @param node
+     */
+    public void insertNode(Tree.Node node) {
+
+    }
+
+    /**
+     * Delete the node from the tree and make sure it is still a BST
+     * Time Complexity :
+     * Space Complexity :
+     * @param node
+     */
+    public void deleteNode(Tree.Node node) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
