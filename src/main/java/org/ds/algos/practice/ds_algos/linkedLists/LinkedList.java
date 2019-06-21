5402c49a9a18068a5b249d3f8949c475c7a0a4bd
Pulkit Gupta <pulgupta@gmail.com>
2019-06-21 11:58:19 +0530

    updated

--- src/main/java/org/ds/algos/practice/ds_algos/linkedLists/LinkedList.java
+++ src/main/java/org/ds/algos/practice/ds_algos/linkedLists/LinkedList.java
@@ -32,10 +32,10 @@ public final class LinkedList {
 	
 	public static Node getLinkedList() {
 		Node node5 = new Node(5, null);
-		Node node4 = new Node(4, node5);
+		Node node4 = new Node(6, node5);
 		Node node3 = new Node(3, node4);
 		Node node2 = new Node(2, node3);
-		Node root = new Node(1, node2);
+		Node root = new Node(5, node2);
 		return root;
 	}	
 	
