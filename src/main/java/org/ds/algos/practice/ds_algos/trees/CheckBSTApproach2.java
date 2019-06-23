3b47459e62060e22f291b434a8745f2ea61d9b08
Pulkit Gupta <pulgupta@gmail.com>
2019-06-23 14:09:38 +0530

    added more problems

---
+++ src/main/java/org/ds/algos/practice/ds_algos/trees/CheckBSTApproach2.java
@@ -0,0 +1,42 @@
+package org.ds.algos.practice.ds_algos.trees;
+
+import static org.ds.algos.practice.ds_algos.trees.Tree.Node;
+
+public class CheckBSTApproach2 {
+    public boolean checkBST_BestWay(Node root, Integer min, Integer max) {
+        if(root == null)
+            return true;
+
+        if ((min!=null && root.data<min) || (max!=null && root.data>max))
+            return false;
+
+        if(!checkBST_BestWay(root.left, min, root.data) || !checkBST_BestWay(root.right, root.data, max)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CheckBSTApproach2 obj = new CheckBSTApproach2();
+        Tree tree = new Tree();
+        // Should return true as this in indeed a BST
+        System.out.println(obj.checkBST_BestWay(tree.createBST(), null, null));
+        /**
+         *          10
+         *         /  \
+         *        5   15
+         *       /
+         *      3
+         *     /
+         *    8
+         */
+        Tree.Node node5 = new Tree.Node(8, null, null);
+        Tree.Node node4 = new Tree.Node(3, node5, null);
+        Tree.Node node3 = new Tree.Node(5, node4, null);
+        Tree.Node node2 = new Tree.Node(15, null, null);
+        Tree.Node node1 = new Tree.Node(10, node3, node2);
+        System.out.println(obj.checkBST_BestWay(node1, null, null));
+
+    }
+}
