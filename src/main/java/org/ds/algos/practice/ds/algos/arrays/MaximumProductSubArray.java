30df83033c3c880d52c26414f4efc3c5c516ff5d
pulgupta <pulgupta@gmail.com>
2020-08-01 13:10:16 +0530

    added a good problem related to recursion

--- src/main/java/org/ds/algos/practice/ds/algos/arrays/MaximumProductSubArray.java
+++ src/main/java/org/ds/algos/practice/ds/algos/arrays/MaximumProductSubArray.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class MaximumProductSubArray {
 
+    /**
+     * The idea is that we will keep track of the min and max at each level. This way at each step we have the
+     * result. It is bottom up way of DP. As we move forward we pick an element and multiply that with the previous
+     * min and max.
+     * @param nums
+     * @return
+     */
     public int maxProduct(int[] nums) {
         int max = nums[0];
         int previousMax = nums[0];
---
+++ src/main/java/org/ds/algos/practice/ds/algos/recursion/GenerateValidParenthesis.java
@@ -0,0 +1,28 @@
+package org.ds.algos.practice.ds.algos.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateValidParenthesis {
+    List<String> result;
+
+    public List<String> generateParenthesis(int n) {
+        result = new ArrayList<>();
+        placeParenthesis("", n, n);
+        return result;
+
+    }
+
+    void placeParenthesis(String sb, int left, int right) {
+        if(left == 0 && right == 0) {
+            result.add(sb);
+        }
+
+        if(left > 0)
+            placeParenthesis(sb + "(", left-1, right);
+
+        // This will ensure that we are not closing a brace before it is opened
+        if(right > left)
+            placeParenthesis(sb + ")", left, right-1);
+    }
+}
