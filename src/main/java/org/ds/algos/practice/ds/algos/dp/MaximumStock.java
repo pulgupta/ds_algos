58363af042ef283724286706f69bb37fe7d70ed2
pulgupta <pulgupta@gmail.com>
2022-01-01 16:02:35 +0530

    cleaned up old problems to be more relevant

--- src/main/java/org/ds/algos/practice/ds/algos/dp/MaximumStock.java
+++ src/main/java/org/ds/algos/practice/ds/algos/dp/MaximumStock.java
@@ -1,46 +1,43 @@
 package org.ds.algos.practice.ds.algos.dp;
-
-import java.util.Scanner;
+/*
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+ * Find the maximum profit you can achieve. You may complete at most k transactions.
+ * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+ */
+import java.util.HashMap;
+import java.util.Map;
 
 /***********SOLVED DUDE ************/
 
 public class MaximumStock {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++) {
+    Map<String, Integer> dp;
+    int k;
+    int[] prices;
+    public int maxProfit(int k, int[] prices) {
+        this.k = k;
+        this.prices = prices;
+        dp = new HashMap<>();
+        return dpFunc(0, k, false);
+    }
 
-            int n = in.nextInt();
-            int[] price = new int[n];
-            for (int j = 0; j < n; j++)
-                price[j] = in.nextInt();
+    private int dpFunc(int i, int t, boolean h) {
+        if(i>=prices.length || t <= 0) return 0;
 
-            //LEDGER: 0 : Do nothing, 1 : buy, 2 : Sell, 
-            int[] ledger = new int[n];
-            long profit = 0;
-            for (int j = 0; j < n; j++) {
-                if (ledger[j] == 0) {
-                    int todayPrice = price[j];
-                    //Find the maximum rate we can sell this
-                    int selling = todayPrice;
-                    int tracker = 0;
-                    for (int k = j; k < n; k++) {
-                        if (selling < price[k]) {
-                            selling = price[k];
-                            tracker = k;
-                        }
-                    }
-                    if (tracker != 0) {
-                        ledger[tracker] = 2;
-                        for (int m = j; m < tracker; m++) {
-                            profit += (price[tracker] - price[m]);
-                            ledger[m] = 1;
-                        }
-                    }
-                }
-            }
-            System.out.println(profit);
+        String key = i + "-" + t + " " + h; // used for memoization
+        if(dp.get(key) != null) return dp.get(key);
+
+        if(h) {
+            // If we are holding the stock then we can do two things and consider the max one
+            // 1. Sell the stock
+            // 2. Do nothing
+            dp.put(key, Math.max(prices[i] + dpFunc(i+1, t-1, false), dpFunc(i+1, t, h)));
+        } else {
+            // If we are not holding the stock then we can do two things and consider the max one
+            // 1. buy the stock
+            // 2. Do nothing
+            dp.put(key, Math.max(-prices[i] + dpFunc(i+1, t, true), dpFunc(i+1, t, h)));
         }
-        in.close();
+        return dp.get(key);
     }
 }
