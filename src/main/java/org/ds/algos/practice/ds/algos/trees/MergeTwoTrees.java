2f8a7271ebc04ffc0bd7d658b89de11c1b723b6a
pulgupta <pulgupta@gmail.com>
2022-12-26 11:19:15 +0530

    added merge two trees program

---
+++ src/main/java/org/ds/algos/practice/ds/algos/trees/MergeTwoTrees.java
@@ -0,0 +1,24 @@
+package org.ds.algos.practice.ds.algos.trees;
+
+import static org.ds.algos.practice.ds.algos.trees.Tree.*;
+
+public class MergeTwoTrees {
+    public Node mergeBinaryTrees(Node tree1, Node tree2) {
+        Node newNode = null;
+        if(tree1 !=null && tree2 != null) {
+            newNode = new Node(tree1.data + tree2.data);
+            newNode.left = mergeBinaryTrees(tree1.left, tree2.left);
+            newNode.right = mergeBinaryTrees(tree1.right, tree2.right);
+        } else if(tree1 != null) {
+            newNode = new Node(tree1.data);
+            newNode.left = mergeBinaryTrees(tree1.left, null);
+            newNode.right = mergeBinaryTrees(tree1.right, null);
+        }
+        else if(tree2!=null) {
+            newNode = new Node(tree2.data);
+            newNode.left = mergeBinaryTrees(null, tree2.left);
+            newNode.right = mergeBinaryTrees(null, tree2.right);
+        }
+        return newNode;
+    }
+}
